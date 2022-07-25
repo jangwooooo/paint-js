4b3efe4fbc1f33f7b37c503941df19e75c9f9588
jangwooooo <s22018@gsm.hs.kr>
2022-07-25 19:40:03 +0900

    1.6 Drawing Project Two

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(170, 170, 15, 100);
+ctx.fillRect(310, 170, 15, 100);
+ctx.fillRect(220, 170, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(230, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
